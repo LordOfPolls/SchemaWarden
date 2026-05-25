2bf3ca24e77967f2737ae87b13bee3294e3aecc7
LordOfPolls <dev@lordofpolls.com>
2026-05-25 19:49:18 +0100

    feat: dynamically get tenant DBs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ mod fetcher;
 mod schema;
 
 use anyhow::Context;
-use tiberius::{AuthMethod, Client, Config};
+use tiberius::{AuthMethod, Client, Config, Query};
 use tokio::net::TcpStream;
 use tokio_util::compat::{Compat, TokioAsyncWriteCompatExt};
 
 const BASELINE_DB: &str = "baseline_db";
-const TENANT_DBS: &[&str] = &["tenant1_db", "tenant2_db"];
 const PWD: &str = "SchemaWarden_Dev1";
 
 #[tokio::main]
@@ -16,9 +15,11 @@ async fn main() -> anyhow::Result<()> {
     let mut baseline_client = connect(BASELINE_DB).await?;
     let baseline = fetcher::fetch_schema(&mut baseline_client, BASELINE_DB).await?;
 
-    for &db in TENANT_DBS {
-        let mut client = connect(db).await?;
-        let tenant = fetcher::fetch_schema(&mut client, db).await?;
+    let tenants = fetch_tenants().await?;
+
+    for db in tenants {
+        let mut client = connect(&db).await?;
+        let tenant = fetcher::fetch_schema(&mut client, &db).await?;
         let drift = diff::diff(&baseline, &tenant);
 
         if drift.is_clean() {
@@ -45,4 +46,35 @@ pub async fn connect(db_name: &str) -> anyhow::Result<Client<Compat<TcpStream>>>
     Client::connect(config, tcp.compat_write())
         .await
         .context("Failed to connect to database")
+}
+
+
+pub async fn fetch_tenants() -> anyhow::Result<Vec<String>> {
+    let mut client = connect("MASTER").await?;
+
+    let sql = "
+        SELECT name
+        FROM sys.databases
+        WHERE name NOT IN (
+                'master'
+                ,'model'
+                ,'msdb'
+                ,'tempdb'
+                )
+            AND STATE = 0
+    ";
+
+    let rows = Query::new(sql)
+        .query(&mut client)
+        .await
+        ?.into_first_result()
+        .await;
+
+    let mut tenants = Vec::new();
+    for row in rows? {
+        let name = row.get::<&str, _>(0).unwrap_or("").to_owned();
+        tenants.push(name);
+    }
+
+    Ok(tenants)
 }
